c362789919bf5eaef68befe75546eb11abd24010
sayedkhaledd <sayedkhaledsa@gmail.com>
2023-10-14 04:55:42 +0300

    initial structure for cinema-booking-backend

---
+++ cinema-room/src/main/java/com/example/cinemaroom/CinemaRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRoomApplication.class, args);
+    }
+
+}
