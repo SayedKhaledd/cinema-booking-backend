a7fa0832704d3114764646a4e91d03497c6c311d
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-03-29 07:22:10 +0200

    small edit

--- cinema-room/src/main/java/com/example/cinemaroom/model/CinemaRoomFilm.java
+++ cinema-room/src/main/java/com/example/cinemaroom/model/CinemaRoomFilm.java
@@ -1,5 +1,10 @@
 package com.example.cinemaroom.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class CinemaRoomFilm {
     private Long id;
     private Film film;
