734a8cee1839c51a0d8e694da6609a2734510127
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-04-12 23:05:59 +0200

    Added testcontainer feature

--- cinema-management/src/test/java/com/example/cinemamanagement/service/CinemaServiceTests.java
+++ cinema-management/src/test/java/com/example/cinemamanagement/service/CinemaServiceTests.java
@@ -3,27 +3,46 @@ package com.example.cinemamanagement.service;
 import com.example.backendcoreservice.BackendCoreServiceApplication;
 import com.example.cinemamanagement.CinemaManagementApplication;
 import com.example.cinemamanagement.dto.CinemaDto;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(classes = {CinemaManagementApplication.class, BackendCoreServiceApplication.class})
 class CinemaServiceTests {
     @Autowired
     private CinemaService cinemaService;
+    private List<CinemaDto> expectedCinemaDtos;
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.2");
+
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
     @Transactional
     @Test
     void testFindAll() {
-        List<CinemaDto> expectedCinemaDtos = CinemaServiceSetup.addRandomCinemas(cinemaService);
+        expectedCinemaDtos = CinemaServiceSetup.addRandomCinemas(cinemaService);
         List<CinemaDto> actualCinemaDtos = cinemaService.findAll();
         assertNotNull(actualCinemaDtos);
         assertEquals(expectedCinemaDtos.size(), actualCinemaDtos.size());
@@ -37,5 +56,10 @@ class CinemaServiceTests {
             assertEquals(expectedCinemaDto.getName(), actualCinemaDto.getName());
             assertEquals(expectedCinemaDto.getLocation(), actualCinemaDto.getLocation());
         });
+    }
+
+    @AfterEach
+    void tearDown() {
+        expectedCinemaDtos.forEach(expectedCinemaDto -> cinemaService.getDao().getRepo().deleteById(expectedCinemaDto.getId()));
     }
 }
